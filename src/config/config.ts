fb7422fc3a4796c944cbac8c6a2b9df440e60284
Ricardo Martinez Osorio <ricardo.martinez@colfive.com>
2022-01-28 15:52:23 -0500

    update endpoint

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,5 +2,5 @@ import { IConfig } from "../interfaces/common";
 
 export const config: IConfig = {
   apiUri: "http://localhost:8090",
-  associationsEndpoint: "/api/associations",
+  associationsEndpoint: "/api/scores",
 };
